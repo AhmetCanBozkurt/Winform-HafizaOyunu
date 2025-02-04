1fa151c4e634ded69ee66374a8c084bfe87f3766
Ahmet Can Bozkurt <ahmetcanb785@gmail.com>
2025-02-05 01:44:51 +0300

    first commit

---
+++ Görsel3RastgeleButton/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Görsel3RastgeleButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "4",
+            "8",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "2",
+            "4",
+            "8",
+            "10"});
+            this.comboBox2.Location = new System.Drawing.Point(246, 22);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "olustur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Satır";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sütün";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(480, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(373, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Skor :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(414, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(579, 83);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Görsel3RastgeleButton/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Görsel3RastgeleButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void doldur(int[] dizi,int boyut)
+        {
+
+            int sayac = 1;
+            for (int i = 0; i < boyut; i++)
+            {
+                dizi[i] = sayac;
+
+                if (sayac == boyut/2)
+                {
+                    sayac = 0;
+
+                }
+                sayac++;
+            }
+
+        }
+        Panel pnl = new Panel();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pnl.Controls.Clear();
+            int boyutX, boyutY, sayac = 0;
+            boyutX = Convert.ToInt32(comboBox1.SelectedItem); //satır
+            boyutY = Convert.ToInt32(comboBox2.SelectedItem);//sütün
+
+            Random rnd = new Random();
+
+            int[] dizi = new int[boyutX* boyutY];
+            doldur(dizi, boyutX * boyutY);
+
+            int[,] matris = new int[boyutX, boyutY];
+
+            int maxDizi = boyutX * boyutY ;
+
+            for (int i = 0; i < boyutX; i++)
+            {
+                for (int j = 0; j < boyutY; j++)
+                {
+                    matris[i, j] = rnd.Next(0, maxDizi);
+
+                    for (int a = 0; a < boyutX; a++)
+                    {
+                        for (int c = 0; c < boyutY; c++)
+                        {
+                            if (matris[i,j] == matris[a,c])
+                            {
+                                sayac++;
+                            }
+
+                          
+                        }
+                    }
+
+                    if (sayac > 1)
+                    {
+                        sayac = 0;
+                        j--;
+                    }
+                }
+            }
+            if (this.Controls.Count == 4)
+            {
+                this.Controls.RemoveAt(3);
+            }
+           
+            pnl.Width = 90 * boyutX;
+            pnl.Height = 70 * boyutY;
+            pnl.BackColor = Color.Red;
+            pnl.Top = 70;
+            pnl.Left = 3;
+            this.Controls.Add(pnl);
+
+            for (int i = 1; i <= boyutX; i++)
+            {
+                for (int t = 1; t <= boyutY; t++)
+                {
+                    Button btn = new Button();
+                    btn.Click += new EventHandler(btn_Click);
+                    btn.BackColor = Color.Blue;
+                    btn.ForeColor = Color.White;
+                    btn.Top = 50 * i;
+                    btn.Left = 50 + t * 60;
+                    btn.Width = 50;
+                    
+                    btn.Text = dizi[matris[i - 1, t - 1]].ToString();
+                    pnl.Controls.Add(btn);
+
+                }
+            }
+        }
+        int sayac = 0;
+        int ilkdeger = 0;
+        int ikincideger = 0;
+        int skor = 0;
+        private Button ilkbuttun;
+
+        private void btn_Click(object sender,EventArgs e)
+        {
+            Button yeni = (sender as Button);
+            //yeni.BackColor = Color.Black;
+
+          
+           
+            if (sayac ==0)
+            {
+                ilkdeger = Convert.ToInt32(yeni.Text);
+                ilkbuttun = yeni;
+                sayac++;
+            }
+            else
+            {
+                 ikincideger = Convert.ToInt32(yeni.Text);
+
+            }
+
+            yeni.Enabled = false;
+
+
+            if (ilkdeger == ikincideger)
+            {
+                yeni.BackColor = Color.Black;
+
+                ilkbuttun.BackColor = Color.Black;
+                sayac = 0;
+                skor++;
+               
+                label4.Text = skor.ToString();
+
+                int alan = Convert.ToInt32(comboBox1.SelectedItem) * Convert.ToInt32(comboBox2.SelectedItem);
+                if (skor==(alan/2))
+                {
+                    MessageBox.Show("Oyunu Kazandınız");
+                    pnl.Controls.Clear();
+                    skor = 0;
+                    
+                }
+
+               
+            }
+           
+        }
+    }
+}
